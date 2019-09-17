4187a8e9d9f0f9d5c1bdfce1d862710ae51d7f85
Jonah Brown <jswish02@gmail.com>
2019-09-17 20:37:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,23 +1,2 @@
-class Shoe
-def initialize(brand)
-  @brand=brand
-end
-def color= (color)
-@color = color
-end
-def color
-  @color
-end
-def size=(size)
-  @size = (size)
-end
-def size
-@size
-end
-def material= (material)
-@material = material
-end
-def material
-  @material
-end
-end
+
+Nike 
